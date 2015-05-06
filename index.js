68f922bb6ade40695ff23b26c24b0950678d95bc
voodoo <voodoo@blackandyellowpages.com>
2015-05-05 18:58:33 -0600

    instantiating socket.io differently

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var app = express();
-var http = require('http').Server(app);
-var io = require('socket.io')(http);
+var server = require('http').createServer(app);
+var io = require('socket.io').listen(server);
 var role = require('./role.js');
 var glob = require('glob');
 
@@ -33,6 +33,6 @@ io.on('connection', function (socket) {
   });
 });
 
-http.listen(3000, function () {
+server.listen(3000, function () {
   console.log('listening on *:3000');
 });
